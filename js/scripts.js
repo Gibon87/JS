d9c4f40850e4b0aed4206e68c4ed499a269445ce
Mateusz <mateusz.gibki@gmail.com>
2017-05-29 22:19:35 +0200

    adding new files from 8.4 task

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+// my line comment.
+
+var variable = 'test';
+
+console.log(variable);
+
+/*
+  Teoretycznie to nie tworzylem nowego repozytorium z nowymi zadaniami ... mysle ze zmiany czesciowe mozemy tutaj nadpisywac 
+  JEsli nie to stworze nowe repozytorium :) 
+*/
